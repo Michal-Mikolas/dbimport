77006eb7bee5be3da04e03a7260a51a7cef34ca9
Michal Mikolas <nanuqcz@gmail.com>
2021-12-23 13:17:55 +0100

    Finished dose-entries import config.

--- config.example.py
+++ config.example.py
@@ -4,19 +4,20 @@ db = 'postgresql://user:pass@server:5432/database'
 table = 'invoices'
 skip = 1
 columns = {
-    'date_from': 'A',
-    'date_to': 'B',
-    'invoice_number': 'C',
-    'order_number': 'D',
-    'insurance': 'E',
-    'icz': ['F', lambda v: int(v.replace('-', '')) if v else None],
-    'created_date': 'G',
-    'payment_date': 'H',
-    'paid_date': 'I',
-    'amount': 'J',
-    'paid_amount': 'K',
-    'points': 'M',
-    'score': 'N',
+	'date_from': 'A',
+	'date_to': 'B',
+	'invoice_number': 'C',
+	'order_number': 'D',
+	'insurance': 'E',
+	'icz': ['F', lambda v: int(v.replace('-', '')) if v else None],
+	'created_date': 'G',
+	'payment_date': 'H',
+	'paid_date': 'I',
+	'amount': 'J',
+	'paid_amount': 'K',
+	'points': 'M',
+	'score': 'N',
 }
 identifiers = ['invoice_number', 'insurance']
 mandatory = ['invoice_number']
+max_empty_rows = 3
--- run.py
+++ run.py
@@ -3,7 +3,8 @@ import openpyxl
 from tools import Tools
 import os
 
-import config_invoices as conf
+import config_dose_entries as conf
+
 
 ######
 #     # #    # #    #
@@ -16,59 +17,67 @@ import config_invoices as conf
 db = dataset.connect(conf.db)
 
 for file in Tools.find_files(f'{conf.inbox}/*.xlsx'):
-    Tools.log(file)
+	Tools.log(file)
 
-    wb = openpyxl.load_workbook(file)
-    ws = wb.active
+	wb = openpyxl.load_workbook(file)
+	ws = wb.active
 
-    r = 0
-    empty_rows_counter = 0
-    while True:
-        #
-        # Init
-        #
-        r += 1
+	r = 0
+	success_counter = 0
+	empty_rows_counter = 0
+	while True:
+		#
+		# Init
+		#
+		r += 1
 
-        if r <= conf.skip:
-            continue
+		#
+		# Get data
+		#
+		entry = {}
+		for name, column in conf.columns.items():
+			if not column:
+				continue
+			elif type(column).__name__ == 'str':
+				column_letter = column
+				column_callback = lambda v: v
+			else:
+				column_letter = column[0]
+				column_callback = column[1]
 
-        #
-        # Get data
-        #
-        entry = {}
-        for name, column in conf.columns.items():
-            if type(column).__name__ == 'str':
-                column_letter = column
-                column_callback = lambda v: v
-            else:
-                column_letter = column[0]
-                column_callback = column[1]
+			v = ws.cell(r, Tools.col_num(column_letter)).value
+			v = column_callback(v)
+			if v:
+				entry[name] = column_callback(v)
 
-            v = ws.cell(r, Tools.col_num(column_letter)).value
-            entry[name] = column_callback(v)
+		#
+		# Skips & Checks
+		#
+		check = True
+		for column in conf.mandatory:
+			if not entry.get(column):
+				check = False
+				empty_rows_counter += 1
 
-        #
-        # Check
-        #
-        check = True
-        for column in conf.mandatory:
-            if not entry[column]:
-                check = False
-                empty_rows_counter += 1
+		if empty_rows_counter >= conf.max_empty_rows:
+			break
 
-        if empty_rows_counter >= 3:
-            break
+		if (type(conf.skip) == int) and (r <= conf.skip):
+			continue
+		if (type(conf.skip).__name__ == 'function') and (conf.skip(entry)):
+			continue
 
-        #
-        # Save to DB
-        #
-        if check:
-            db[conf.table].upsert(entry, conf.identifiers)
+		#
+		# Save to DB
+		#
+		if check:
+			db[conf.table].upsert(entry, conf.identifiers)
+			success_counter += 1
 
-    outbox_path = f'{conf.outbox}/{Tools.remove_dirs(file)}'
-    os.unlink(outbox_path) if os.path.isfile(outbox_path) else None
-    os.rename(file, outbox_path)
+	outbox_path = f'{conf.outbox}/{Tools.remove_dirs(file)}'
+	os.unlink(outbox_path) if os.path.isfile(outbox_path) else None
+	os.rename(file, outbox_path)
 
-    Tools.log(f'...done {r-conf.skip-3} rows\n')
+	Tools.log(f'...done {success_counter} rows\n')
 
 Tools.log('Done.')
